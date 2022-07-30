c3fa5bc3e96ed0eea3671f52ec0e48a062648c4a
Sam Fernandez <sfdz@users.noreply.github.com>
2022-07-30 16:13:28 -0400

    Add rinkeby config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,5 @@
+import { infuraAPIKey, mnemonic } from "./secrets.json";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
@@ -5,6 +7,12 @@ import "@openzeppelin/hardhat-upgrades"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${infuraAPIKey}`,
+      accounts: { mnemonic }
+    }
+  }
 };
 
 export default config;
